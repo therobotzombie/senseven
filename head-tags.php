b5327282d2b3d7d3111fc112ad7906eaa4c7be3c
Laura Redeker <lauraaredeker@gmail.com>
2016-04-29 13:31:49 +0200

    fixes navigation etc.
    navigation gefixt, projektansicht ergänzt und angepasst (jetzt gibt es
    leere felder, da es nicht aufgeht), nach-oben-button, "danke"-bereich
    angelegt, favicon erstellt...

--- head-tags.php
+++ head-tags.php
@@ -2,19 +2,53 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
-    <!-- Facebook and Twitter integration -->
-	<meta property="og:title" content="SENSEVEN - Die Ausstellung mit dem siebten Sinn. Kunst und Multimedia VII"/>
-	<meta property="og:image" content=""/>
-	<meta property="og:url" content=""/>
-	<meta property="og:site_name" content=""/>
-	<meta property="og:description" content=""/>
-	<meta name="twitter:title" content="" />
-	<meta name="twitter:image" content="" />
-	<meta name="twitter:url" content="" />
-	<meta name="twitter:card" content="" />
+    <!-- Meta Tags -->
+    <meta name="description" lang="DE" 
+    content="Unter dem Titel SENSEVEN eröffnet der siebte Jahrgang des Studiengangs Kunst und Multimedia am 15. Juli 2016 seine Abschlussausstellung im Studio 2 des Muffatwerks in München.">
+    <meta name="author" content="Kunst und Multimedia 7">
+    <meta name="keywords" lang="DE" content="SENSEVEN, ausstellung, sieben sinne, münchen, 2016, muffatwerk, kunst und multimedia, projektarbeit, abschlussjahrgang 2016">
+    <meta name="robots" content="index, follow">
+    <meta name="copyright" content="Kunst und Multimedia 7">
+    <meta name="language" content="DE">
+    <link rel="canonical" href="http://www.senseven.net">
+
+    <!-- Social: Twitter -->
+    <meta name="twitter:card" content="photo">
+    <meta name="twitter:url" content="http://www.senseven.net/" />
+    <meta name="twitter:title" content="SENSEVEN - Die Ausstellung mit dem siebten Sinn. Kunst und Multimedia 7">
+    <meta name="twitter:description" content="Unter dem Titel SENSEVEN eröffnet der siebte Jahrgang des Studiengangs 
+    Kunst und Multimedia am 15. Juli 2016 seine Abschlussausstellung im Studio 2 des Muffatwerks in München.">
+    <meta name="twitter:image" content="http://senseven.net/img/logo_senseven_700.jpg">
+    
+    <!-- Social: Facebook / Open Graph -->
+    <meta property="fb:admins" content=""> <!-- TODO ? -->
+    <meta property="og:title" content="SENSEVEN - Die Ausstellung mit dem siebten Sinn. Kunst und Multimedia 7">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="http://www.senseven.net">
+    <meta property="og:image" content="http://senseven.net/img/logo_senseven_700.jpg">
+    <meta property="og:description" content="Unter dem Titel SENSEVEN eröffnet der siebte Jahrgang des Studiengangs 
+    Kunst und Multimedia am 15. Juli 2016 seine Abschlussausstellung im Studio 2 des Muffatwerks in München.">
+    
+    <!-- Social: Google+ / Schema.org  -->
+    <link rel="author" href="Kunst und Multimedia 7">
+    <link rel="publisher" href="Kunst und Multimedia 7">
+    <meta itemprop="name" content="SENSEVEN - Die Ausstellung mit dem siebten Sinn. Kunst und Multimedia 7">
+    <meta itemprop="description" content="Unter dem Titel SENSEVEN eröffnet der siebte Jahrgang des Studiengangs Kunst und 
+    Multimedia am 15. Juli 2016 seine Abschlussausstellung im Studio 2 des Muffatwerks in München.">
+    <meta itemprop="image" content="http://senseven.net/img/logo_senseven_700.jpg">
 
 	<!-- Place favicon.ico in the root directory -->
-	<link rel="shortcut icon" href="favicon.ico">
+	<link rel="shortcut icon" href="favicon.ico"><br>
+
+    <!-- Favicons  TODO-->
+    <link rel="shortcut icon" sizes="16x16 24x24 32x32 48x48 64x64" href="./img/icons/favicon.png">
+    <link rel="apple-touch-icon" sizes="57x57" href="./img/icons/favicon-57.png">
+    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="./img/icons/favicon-57.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="./img/icons/favicon-72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="./img/icons/favicon-114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="./img/icons/favicon-120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="./img/icons/favicon-144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="./img/icons/favicon-152.png">
 
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700|Lato:100,400,300,700,900' rel='stylesheet' type='text/css'>
