768cb17d6b20c1e097c54f9de080df5d99679145
rejoin <ununchanged@yahoo.com>
2016-05-02 22:58:07 +0200

    aww die files warend wohl alle nichz dabei =P

---
+++ include/modals/modalbasic.php
@@ -0,0 +1,29 @@
+<!--  MODAL ANNIKA ----->
+
+<div class="modal modal-fullscreen fade" id="modalbasic" tabindex="-1" role="dialog">
+  <div class="modal-dialog">
+    <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h1 id="modalHeader"></h1>
+                <span class="underline-white"> </span>
+                <h3 id="modalName"></h3>
+                <img id="modalImg" class="modal-img" src="" alt="Project">
+          </div>
+
+          <div class="modal-body">
+                <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <p id="modalText">
+                                </p>
+                            </div>
+                        </div> <!-- / row -->
+                    </div>
+              </div>
+        <div class="modal-footer">
+        </div>
+
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
