ca4d2cf5a0fd93e8ec7dd5cb5c91a10a37bf8f64
rejoin <ununchanged@yahoo.com>
2016-05-02 20:54:04 +0200

    Die restlichen modals, es fehlt natürlich noch einiges zwecks trennung und layout - überschrift ist etwas schepps und bei drei leuten dfindich den text nich =P muss nachfragen

--- include/projekte.php
+++ include/projekte.php
@@ -11,7 +11,7 @@
         <div class="col-lg-12 text-center">
             <ul class="cbp-rfgrid">
                 <li>
-                    <a data-toggle="modal" data-target="#modalbasic" data-projekt-id="mittelm"><img src="img/project_mittelm.jpg" />bla<div><h3>Annika Mittelmeier</h3></div></a>
+                    <a data-toggle="modal" data-target="#modalbasic" data-projekt-id="mittelm"><img src="img/project_mittelm.jpg" /><div><h3>Annika Mittelmeier</h3></div></a>
                 </li>   
                 <li>
                     <a data-toggle="modal" data-target="#modalbasic" data-projekt-id="bardosi"><img src="img/project_bardosi.jpg" /><div><h3>Vivien Bardosi</h3></div></a>
@@ -20,10 +20,10 @@
                 	<a data-toggle="modal" data-target="#modalbasic" data-projekt-id="biersack"><img src="img/project_biersack.jpg" /><div><h3>Teresa Biersack</h3></div></a>
                 </li>
                 <li>
-                	<a data-toggle="modal" data-target="#project-aurbach"><img src="img/project_aurbach.jpg" /><div><h3>Alina Aurbach</h3></div></a>
+                	<a data-toggle="modal" data-target="#modalbasic" data-projekt-id="aurbach"><img src="img/project_aurbach.jpg" /><div><h3>Alina Aurbach</h3></div></a>
                 </li>
                 <li>
-                	<a data-toggle="modal" data-target="#project-frohwein"><img src="img/project_frohwein.jpg" /><div><h3>Daniel Frohwein</h3></div></a>
+                	<a data-toggle="modal" data-target="#modalbasic" data-projekt-id="frohwein"><img src="img/project_frohwein.jpg" /><div><h3>Daniel Frohwein</h3></div></a>
                 </li>
                 <li>
                 	<a class="empty hidden-xs"><img src="img/project_empty.png" /></a>
@@ -32,7 +32,7 @@
                 	<a data-toggle="modal" data-target="#modalbasic" data-projekt-id="heinemann"><img src="img/project_heinemann.jpg" /><div><h3>Michèle Heinemann</h3></div></a>
                 </li>
                 <li>
-                	<a data-toggle="modal" data-target="#project-dittrich"><img src="img/project_dittrich.jpg" /><div><h3>Svenja Dittrich</h3></div></a>
+                	<a data-toggle="modal" data-target="#modalbasic" data-projekt-id="dittrich"><img src="img/project_dittrich.jpg" /><div><h3>Svenja Dittrich</h3></div></a>
                 </li>
                 <li>
                 	<a data-toggle="modal" data-target="#modalbasic" data-projekt-id="kurbanova"><img src="img/project_kurbanova.jpg" /><div><h3>Irina Kurbanova</h3></div></a>
@@ -41,16 +41,16 @@
                 	<a data-toggle="modal" data-target="#modalbasic" data-projekt-id="ponikowski"><img src="img/project_ponikowski.jpg" /><div><h3>Lukas Ponikowski</h3></div></a>
                 </li>     
                 <li>
-                	<a data-toggle="modal" data-target="#project-redeker"><img src="img/project_redeker.jpg" /><div><h3>Laura Redeker</h3></div></a>
+                	<a data-toggle="modal" data-target="#modalbasic" data-projekt-id="redeker"><img src="img/project_redeker.jpg" /><div><h3>Laura Redeker</h3></div></a>
                 </li>
                 <li>
                 	<a class="empty hidden-xs"><img src="img/project_empty.png" /></a>
                 </li>         
                 <li>
-                	<a data-toggle="modal" data-target="#project-wang"><img src="img/project_wang.jpg" /><div><h3>Robin Wang</h3></div></a>
+                	<a data-toggle="modal" data-target="#modalbasic" data-projekt-id="wang"><img src="img/project_wang.jpg" /><div><h3>Robin Wang</h3></div></a>
                 </li>  
                 <li>
-                	<a data-toggle="modal" data-target="#project-binder"><img src="img/project_binder.jpg" /><div><h3>Katharina Binder</h3></div></a>
+                	<a data-toggle="modal" data-target="#modalbasic" data-projekt-id="binder"><img src="img/project_binder.jpg" /><div><h3>Katharina Binder</h3></div></a>
                 </li>  
                 <li>
                 	<a data-toggle="modal" data-target="#modalbasic" data-projekt-id="korbanka"><img src="img/project_korbanka.jpg" /><div><h3>Lasse Korbanka</h3></div></a>
@@ -62,13 +62,13 @@
                 	<a class="empty hidden-xs"><img src="img/project_empty.png" /></a>
                 </li>         
                 <li>
-                	<a data-toggle="modal" data-target="#project-rapp"><img src="img/project_rapp.jpg" /><div><h3>Padraic Rapp</h3></div></a>
+                	<a data-toggle="modal" data-target="#modalbasic" data-projekt-id="rapp"><img src="img/project_rapp.jpg" /><div><h3>Padraic Rapp</h3></div></a>
                 </li>
                 <li>
-                	<a data-toggle="modal" data-target="#project-woods"><img src="img/project_woods.jpg" /><div><h3>Jessica Woods</h3></div></a>
+                	<a data-toggle="modal" data-target="#modalbasic" data-projekt-id="woods"><img src="img/project_woods.jpg" /><div><h3>Jessica Woods</h3></div></a>
                 </li>        
                 <li>
-                	<a data-toggle="modal" data-target="#project-schmidt"><img src="img/project_schmidt.jpg" /><div><h3>Patrizia Schmidt</h3></div></a>
+                	<a data-toggle="modal" data-target="#modalbasic" data-projekt-id="schmidt"><img src="img/project_schmidt.jpg" /><div><h3>Patrizia Schmidt</h3></div></a>
                 </li>        
             </ul>
         </div>
