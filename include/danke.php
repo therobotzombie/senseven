d036675ebd5caf3fee061b240de4c44cf074b14b
Laura Redeker <lauraaredeker@gmail.com>
2016-05-03 21:33:43 +0200

    sponsoren bereich angefangen

--- include/danke.php
+++ include/danke.php
@@ -7,19 +7,48 @@
                  <h4>für die finanzielle und tatkräftige Unterstützung</h4>
                  
                  <p>
-                  Wir danken dem Department Kunstwissenschaften sowie den Instituten für Kunstpädagogik und Medieninformatik der LMU München. 
-                  Desweiteren danken wir dem Team der Mohr Villa Freimann für die Bereitstellung ihrer Ausstellungsräume, den Sponsoren für ihre Spenden, 
-                  unseren Familien und Freunden und allen weiteren Unterstützern. <br><br>
-
-                    Wir danken Frau Dr. Brigitte Kaiser für ihre Betreuung unserer Ausstellungorganisation. Recht herzlich bedanken wir uns auch bei Frau 
-                    Dr. Karin Guminski, Leiterin des Studiengangs Kunst und Multimedia, die uns im Laufe des Studiums und insbesondere während unseres Projektjahres 
-                    beratend zur Seite gestanden hat.
-					<br>
-                    <br>
-                    SPONSOREN LOGOS
-                </p>
-
-            </div>
+                      Wir danken dem Department Kunstwissenschaften sowie den Instituten für Kunstpädagogik und Medieninformatik der LMU München. 
+                      Desweiteren danken wir dem Team der Mohr Villa Freimann für die Bereitstellung ihrer Ausstellungsräume, den Sponsoren für ihre Spenden, 
+                      unseren Familien und Freunden und allen weiteren Unterstützern. <br><br>
+    
+                        Wir danken Frau Dr. Brigitte Kaiser für ihre Betreuung unserer Ausstellungorganisation. Recht herzlich bedanken wir uns auch bei Frau 
+                        Dr. Karin Guminski, Leiterin des Studiengangs Kunst und Multimedia, die uns im Laufe des Studiums und insbesondere während unseres Projektjahres 
+                        beratend zur Seite gestanden hat.
+                        <br/>
+                        <br/>
+                   </p>
+             </div>
         </div>
+        
+        <div class="sponsoren margin-top text-center">
+       			<h4></h4>
+                <div class="row margin-top">                  
+                     <div class="col-sm-6 col-md-4">
+                        <a href=""><img src="img/sponsoren/hypokulturstifung.png" alt="hypo-kulturstifung" class="col-xs-12 img-reponsive" /></a>
+                        <a href="http://www.lfa.de/Förderbank"><img src="img/sponsoren/LfA_Logo.jpg" alt="hypo-kulturstifung" class="col-xs-12 img-reponsive" /></a>
+                     </div>
+                     <div class="col-sm-6 col-md-3">
+                        <a href=""><img src="img/sponsoren/gaertnerplatz_openair.jpg" alt="gärtnerplatz openair 2016" class="col-xs-12 img-reponsive" /></a>
+                     </div>
+                     <div class="col-sm-6 col-md-3">
+                      	 <a href=""><img src="img/sponsoren/subseq.jpg" alt="SUBSEQ.net" class="col-xs-12 img-reponsive" /></a>
+                     </div>
+                     <div class="col-sm-6 col-md-2">
+                          <a href="http://www.muffatwerk.de/de/"><img src="img/sponsoren/mh.jpg" alt="Muffatwerk" class="col-xs-12 img-reponsive" /></a>
+                     </div>
+                     </div>
+                <div class="row">     
+                     <div class="col-sm-6 col-md-3">
+                        <a href="https://www.uni-muenchen.de/"><img src="img/sponsoren/lmu.png" alt="LMU München" class="col-xs-12 img-reponsive" /></a>
+                     </div>
+                     <div class="col-sm-6 col-md-2">
+                        <a href=""><img src="img/sponsoren/BardosiTransporte_logo.png" alt="Bardosi Transporte" class="col-xs-12 img-reponsive" /></a>
+                     </div>
+                     <div class="col-sm-6 col-md-2">
+                        <a href="http://harrykleinclub.de/"><img src="img/sponsoren/harryklein.png" alt="Harry Klein" class="col-xs-12 img-reponsive" /></a>
+                     </div>    
+                </div>
+		</div>
+        
     </div>
 </div>
