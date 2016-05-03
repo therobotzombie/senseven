4794a6159439c2c58dbc75c330f89ad28b337316
Laura Redeker <lauraaredeker@gmail.com>
2016-05-03 12:41:08 +0200

    projektansicht, überflüssige dateien gechlöscht
    projektansicht für mobile angepasst, modals css angespasst +
    designvorschlag :)

--- include/modals/modalbasic.php
+++ include/modals/modalbasic.php
@@ -1,29 +1,25 @@
-<!--  MODAL ANNIKA ----->
-
+<!--  MODAL BASIC -->
 <div class="modal modal-fullscreen fade" id="modalbasic" tabindex="-1" role="dialog">
+<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
   <div class="modal-dialog">
     <div class="modal-content">
           <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h1 id="modalHeader"></h1>
-                <span class="underline-white"> </span>
-                <h3 id="modalName"></h3>
                 <img id="modalImg" class="modal-img" src="" alt="Project">
           </div>
-
           <div class="modal-body">
                 <div class="container-fluid">
                         <div class="row">
+                            <h1 id="modalHeader"></h1>
+                			<h3 id="modalName"></h3>
                             <div class="col-sm-12">
                                 <p id="modalText">
                                 </p>
                             </div>
-                        </div> <!-- / row -->
+                        </div>
                     </div>
               </div>
         <div class="modal-footer">
         </div>
-
     </div><!-- /.modal-content -->
   </div><!-- /.modal-dialog -->
 </div><!-- /.modal -->
