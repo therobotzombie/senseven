92634268d7aa0e4b55e6ee2f1f8c6fe219dc50c1
Laura Redeker <lauraaredeker@gmail.com>
2016-06-01 12:37:24 +0200

    feinanpassungen text mobile/tablet

--- include/kontakt.php
+++ include/kontakt.php
@@ -1,7 +1,7 @@
 <div class="content-bg">
 <div class="container">
 <div class="row">
-    <div class="col-lg-8 col-lg-offset-2 text-center">
+    <div class="col-md-12 col-lg-8 col-lg-offset-2 text-center">
         <h1>Kontakt</h1>
         <span class="underline-white"></span><br/>
 
@@ -11,15 +11,14 @@
         	 <a class="btn btn-link" data-toggle="modal" data-target="#pressemodal">Pressematerial</a>
 
         </p>
-
-        
-        <div class="row margin-top">
-        	<h4>Ansprechpartner</h4>
-            <div class="col-xs-6 text-right border-right link-list">
+     
+        <div class="row margin-top hidden-xs">
+        	<h5>Ansprechpartner</h5>
+            <div class="col-sm-6 text-right border-right link-list">
                 <p>
                     <strong>Projektmanagement</strong><br/>
                     <strong>Betreuende Dozenten</strong><br/>
-                    <strong>Presse-&amp; &Ouml;ffentlichkeitsarbeit</strong><br/>
+                    <strong class="hidden-xs">Presse-&amp; &Ouml;ffentlichkeitsarbeit</strong><br/>
                     <strong>Sponsoren &amp; Finanzen</strong><br/>
                     <strong>Ausstellungsorganisation</strong><br>
                     <strong>Catering</strong><br>
@@ -28,7 +27,7 @@
                     <strong>Webdesign</strong>
                 </p>
             </div>
-            <div class="col-xs-6 text-left link-list">
+            <div class="col-sm-6 text-left link-list">
                 <p>
                     <a href="mailto:svenja.dittrich@web.de">Svenja Dittrich, </a><a href="mailto:lukas@ponikowski.de@web.de">Lukas Ponikowski</a><br/>
                     <a href="mailto:karin.guminski@lmu.de">Dr. Karin Guminski, </a><a href="mailto:brigitte-kaiser@gmx.de">Dr. Brigitte Kaiser</a><br/>
@@ -42,10 +41,28 @@
                 </p>
             </div>
         </div>
+        
+        <div class="row margin-top visible-xs">
+        	<h5>Ansprechpartner</h5>
+            <div class="col-xs-12 link-list">
+                <p>
+                    <strong>Projektmanagement: </strong><a href="mailto:svenja.dittrich@web.de">Svenja Dittrich, </a><a href="mailto:lukas@ponikowski.de@web.de">Lukas Ponikowski</a><br/>
+                    <strong>Betreuende Dozenten: </strong><a href="mailto:karin.guminski@lmu.de">Dr. Karin Guminski, </a><a href="mailto:brigitte-kaiser@gmx.de">Dr. Brigitte Kaiser</a><br/>
+                    <strong>PR: </strong><a href="mailto:lasse-korbanka@web.de">Lasse Korbanka</a><br/>
+                    <strong>Sponsoren &amp; Finanzen: </strong><a href="mailto:vivien.bardosi@gmail.com">Vivien Bardosi</a><br/>
+                    <strong>Ausstellungsorganisation: </strong><a href="mailto:svenja.dittrich@web.de">Svenja Dittrich</a><br/>
+                    <strong>Catering: </strong><a href="mailto:Teresa.b94@gmx.de@campus.lmu.de">Teresa Biersack</a><br/>
+                    <strong>Begleitprogramm: </strong><a href="mailto:Katharina.Binder1@gmx.de">Katharina Binder</a><br/>
+                    <strong>Printdesign: </strong><a href="mailto:Annika.Mittelmeier@campus.lmu.de">Annika Mittelmeier</a><br/>
+                    <strong>Webdesign: </strong><a href="mailto:lauraaredeker@gmail.com">Laura Redeker</a>
+                </p>
+            </div>
+        </div>
 
+        
     <div class="row margin-top">
      	<!-- adding the names dynamically since the list gets so long, the code is in modalx.js, prolly should rename the file --->
-        <h4>Ausstellende K&uuml;nstler</h4>
+        <h5>Ausstellende K&uuml;nstler</h5>
         <div class="row">
         	<div id="namensliste" class="link-list"></div>
         </div>
