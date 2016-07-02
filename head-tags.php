07a392561946e80b5f4c1c9313c86b8ac3733a76
luk-pon <lukas.ponikowski@gmail.com>
2016-07-02 17:24:21 +0200

    headt-tags.php bereinigt; alt-Attribute ergänzt
    Umbruch entfernt

--- head-tags.php
+++ head-tags.php
@@ -33,14 +33,11 @@ Kunst und Multimedia am 15. Juli 2016 seine Abschlussausstellung im Studio 2 des
 <link rel="author" href="Kunst%20und%20Multimedia%207">
 <link rel="publisher" href="Kunst%20und%20Multimedia%207">
 <meta itemprop="name" content="SENSEVEN - Die Ausstellung mit dem siebten Sinn. Kunst und Multimedia 7">
-<meta itemprop="description" content="Unter dem Titel SENSEVEN eröffnet der siebte Jahrgang des Studiengangs Kunst und 
-Multimedia am 15. Juli 2016 seine Abschlussausstellung im Studio 2 des Muffatwerks in München.">
+<meta itemprop="description" content="Unter dem Titel SENSEVEN eröffnet der siebte Jahrgang des Studiengangs Kunst und Multimedia am 15. Juli 2016 seine Abschlussausstellung im Studio 2 des Muffatwerks in München.">
 <meta itemprop="image" content="http://senseven.net/img/logo_senseven_fb.png">
 
 <!-- Place favicon.ico in the root directory -->
-<link rel="shortcut icon" href="favicon.ico"><br>
-
-<!-- Favicons  TODO-->
+<link rel="shortcut icon" href="favicon.ico">
 <link rel="shortcut icon" sizes="16x16 24x24 32x32 48x48 64x64" href="./img/icons/favicon.png">
 <link rel="apple-touch-icon" sizes="57x57" href="./img/icons/favicon-57.png">
 <link rel="apple-touch-icon-precomposed" sizes="57x57" href="./img/icons/favicon-57.png">
@@ -61,4 +58,4 @@ Multimedia am 15. Juli 2016 seine Abschlussausstellung im Studio 2 des Muffatwer
 <!--[if lt IE 9]>
 <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-<![endif]-->    
+<![endif]-->
