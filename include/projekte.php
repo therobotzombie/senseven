70c84aa292d3750df10546cb175bd2e3205873c1
luk-pon <lukas.ponikowski@gmail.com>
2016-07-02 17:29:23 +0200

    data-scr Attribute um src ergänzt
    Element img is missing required attribute src.

--- include/projekte.php
+++ include/projekte.php
@@ -13,82 +13,82 @@
 
                 <li>
                     <a data-toggle="modal" data-target="#modalbasic" data-projekt-id="mittelm" title="Projektdetails öffnen">
-                    <img class="lazy" data-src="img/projekte/project_mittelm.jpg" alt="Projektvorschau" /><div><h3>Annika Mittelmeier</h3></div></a>
+                    <img class="lazy" data-src="img/projekte/project_mittelm.jpg" src="img/projekte/project_mittelm.jpg" alt="Projektvorschau" /><div><h3>Annika Mittelmeier</h3></div></a>
                 </li>   
                 <li>
                     <a data-toggle="modal" data-target="#modalbasic" data-projekt-id="bardosi" title="Projektdetails öffnen">
-                    <img class="lazy" data-src="img/projekte/project_bardosi.jpg" alt="Projektvorschau" /><div><h3>Vivien Bardosi</h3></div></a>
+                    <img class="lazy" data-src="img/projekte/project_bardosi.jpg" src="img/projekte/project_bardosi.jpg" alt="Projektvorschau" /><div><h3>Vivien Bardosi</h3></div></a>
                 </li>
                 
                 <li>
                 	<a data-toggle="modal" data-target="#modalbasic" data-projekt-id="biersack" title="Projektdetails öffnen">
-                    <img class="lazy" data-src="img/projekte/project_biersack.jpg" alt="Projektvorschau" /><div><h3>Teresa Biersack</h3></div></a>
+                    <img class="lazy" data-src="img/projekte/project_biersack.jpg" src="img/projekte/project_biersack.jpg" alt="Projektvorschau" /><div><h3>Teresa Biersack</h3></div></a>
                 </li> 
 
                 <li>
                 	<a data-toggle="modal" data-target="#modalbasic" data-projekt-id="frohwein" title="Projektdetails öffnen">
-                    <img class="lazy" data-src="img/projekte/project_frohwein.jpg" alt="Projektvorschau" /><div><h3>Daniel Frohwein</h3></div></a>
+                    <img class="lazy" data-src="img/projekte/project_frohwein.jpg" src="img/projekte/project_frohwein.jpg" alt="Projektvorschau" /><div><h3>Daniel Frohwein</h3></div></a>
                 </li> 
                     
                 <li>
                 	<a data-toggle="modal" data-target="#modalbasic" data-projekt-id="heinemann" title="Projektdetails öffnen">
-                    <img class="lazy" data-src="img/projekte/project_heinemann.jpg" alt="Projektvorschau" /><div><h3>Michèle Heinemann</h3></div></a>
+                    <img class="lazy" data-src="img/projekte/project_heinemann.jpg" src="img/projekte/project_heinemann.jpg" alt="Projektvorschau" /><div><h3>Michèle Heinemann</h3></div></a>
                 </li>
                 <li>
-                	<a class="empty hidden-xs hidden-sm hidden-md hidden-lg visible-lgplus"><img class="lazy" data-src="img/projekte/project_empty.png" alt="Projektvorschau" /></a>
+                	<a class="empty hidden-xs hidden-sm hidden-md hidden-lg visible-lgplus"><img class="lazy" data-src="img/projekte/project_empty.png" src="img/projekte/project_empty.png" alt="Projektvorschau" /></a>
                 </li>  
                 <li>
                 	<a data-toggle="modal" data-target="#modalbasic" data-projekt-id="dittrich" title="Projektdetails öffnen">
-                    <img class="lazy" data-src="img/projekte/project_dittrich.jpg" alt="Projektvorschau" /><div><h3>Svenja Dittrich</h3></div></a>
+                    <img class="lazy" data-src="img/projekte/project_dittrich.jpg" src="img/projekte/project_dittrich.jpg" alt="Projektvorschau" /><div><h3>Svenja Dittrich</h3></div></a>
                 </li>  
                 <li>
                 	<a data-toggle="modal" data-target="#modalbasic" data-projekt-id="kurbanova" title="Projektdetails öffnen">
-                    <img class="lazy" data-src="img/projekte/project_kurbanova.jpg" alt="Projektvorschau" /><div><h3>Irina Kurbanova</h3></div></a>
+                    <img class="lazy" data-src="img/projekte/project_kurbanova.jpg" src="img/projekte/project_kurbanova.jpg" alt="Projektvorschau" /><div><h3>Irina Kurbanova</h3></div></a>
                 </li>
                 <li>
                 	<a data-toggle="modal" data-target="#modalbasic" data-projekt-id="ponikowski" title="Projektdetails öffnen">
-                    <img class="lazy" data-src="img/projekte/project_ponikowski.jpg" alt="Projektvorschau" /><div><h3>Lukas Ponikowski</h3></div></a>
+                    <img class="lazy" data-src="img/projekte/project_ponikowski.jpg" src="img/projekte/project_ponikowski.jpg" alt="Projektvorschau" /><div><h3>Lukas Ponikowski</h3></div></a>
                 </li>     
                 <li>
                 	<a data-toggle="modal" data-target="#modalbasic" data-projekt-id="redeker" title="Projektdetails öffnen">
-                    <img class="lazy" data-src="img/projekte/project_redeker.jpg" alt="Projektvorschau" /><div><h3>Laura Redeker</h3></div></a>
+                    <img class="lazy" data-src="img/projekte/project_redeker.jpg" src="img/projekte/project_redeker.jpg" alt="Projektvorschau" /><div><h3>Laura Redeker</h3></div></a>
                 </li>   
                  
                 <li>
                 	<a data-toggle="modal" data-target="#modalbasic" data-projekt-id="wang" title="Projektdetails öffnen">
-                    <img class="lazy" data-src="img/projekte/project_wang.jpg" alt="Projektvorschau" alt="Projektvorschau" /><div><h3>Robin Wang</h3></div></a>
+                    <img class="lazy" data-src="img/projekte/project_wang.jpg" src="img/projekte/project_wang.jpg" alt="Projektvorschau" alt="Projektvorschau" /><div><h3>Robin Wang</h3></div></a>
                 </li>  
 
                 <li>
                 	<a data-toggle="modal" data-target="#modalbasic" data-projekt-id="binder" title="Projektdetails öffnen">
-                    <img class="lazy" data-src="img/projekte/project_binder.jpg" alt="Projektvorschau" /><div><h3>Katharina Binder</h3></div></a>
+                    <img class="lazy" data-src="img/projekte/project_binder.jpg" src="img/projekte/project_binder.jpg" alt="Projektvorschau" /><div><h3>Katharina Binder</h3></div></a>
                 </li> 
                 <li>
                 	<a data-toggle="modal" data-target="#modalbasic" data-projekt-id="aurbach" title="Projektdetails öffnen">
-                    <img class="lazy" data-src="img/projekte/project_aurbach.jpg" alt="Projektvorschau" /><div><h3>Alina Aurbach</h3></div></a>
+                    <img class="lazy" data-src="img/projekte/project_aurbach.jpg" src="img/projekte/project_aurbach.jpg" alt="Projektvorschau" /><div><h3>Alina Aurbach</h3></div></a>
                 </li>
                 <li>
                 	<a data-toggle="modal" data-target="#modalbasic" data-projekt-id="korbanka" title="Projektdetails öffnen">
-                    <img class="lazy" data-src="img/projekte/project_korbanka.jpg" alt="Projektvorschau" /><div><h3>Lasse Korbanka</h3></div></a>
+                    <img class="lazy" data-src="img/projekte/project_korbanka.jpg" src="img/projekte/project_korbanka.jpg" alt="Projektvorschau" /><div><h3>Lasse Korbanka</h3></div></a>
                 </li>              
                 <li>
                 	<a data-toggle="modal" data-target="#modalbasic" data-projekt-id="scavuzzo" title="Projektdetails öffnen">
-                    <img class="lazy" data-src="img/projekte/project_scavuzzo.jpg" alt="Projektvorschau" /><div><h3>Christian Scavuzzo</h3></div></a>
+                    <img class="lazy" data-src="img/projekte/project_scavuzzo.jpg" src="img/projekte/project_scavuzzo.jpg" alt="Projektvorschau" /><div><h3>Christian Scavuzzo</h3></div></a>
                 </li> 
                 <li>
                 	<a data-toggle="modal" data-target="#modalbasic" data-projekt-id="rapp" title="Projektdetails öffnen">
-                    <img class="lazy" data-src="img/projekte/project_rapp.jpg" alt="Projektvorschau" /><div><h3>Padraic Rapp</h3></div></a>
+                    <img class="lazy" data-src="img/projekte/project_rapp.jpg" src="img/projekte/project_rapp.jpg" alt="Projektvorschau" /><div><h3>Padraic Rapp</h3></div></a>
                 </li>
                 <li>
-                	<a class="empty  hidden-xs hidden-sm hidden-md hidden-lg visible-lgplus"><img class="lazy" data-src="img/projekte/project_empty.png" alt="Projektvorschau"/></a>
+                	<a class="empty  hidden-xs hidden-sm hidden-md hidden-lg visible-lgplus"><img class="lazy" data-src="img/projekte/project_empty.png" src="img/projekte/project_empty.png" alt="Projektvorschau"/></a>
                 </li>   
                 <li>
                 	<a data-toggle="modal" data-target="#modalbasic" data-projekt-id="woods" title="Projektdetails öffnen">
-                    <img class="lazy" data-src="img/projekte/project_woods.jpg" alt="Projektvorschau" /><div><h3>Jessica Woods</h3></div></a>
+                    <img class="lazy" data-src="img/projekte/project_woods.jpg" src="img/projekte/project_woods.jpg" alt="Projektvorschau" /><div><h3>Jessica Woods</h3></div></a>
                 </li>          
                 <li>
                 	<a data-toggle="modal" data-target="#modalbasic" data-projekt-id="schmidt" title="Projektdetails öffnen">
-                    <img class="lazy" data-src="img/projekte/project_schmidt.jpg" alt="Projektvorschau" /><div><h3>Patrizia Schmidt</h3></div></a>
+                    <img class="lazy" data-src="img/projekte/project_schmidt.jpg" src="img/projekte/project_schmidt.jpg" alt="Projektvorschau" /><div><h3>Patrizia Schmidt</h3></div></a>
                 </li>        
             </ul>
         </div>
