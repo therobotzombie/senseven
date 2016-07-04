685b48a2dac57e024a2c56556824e83021361914
Laura Redeker <lauraaredeker@gmail.com>
2016-07-04 12:00:28 +0200

    Konflikte gelöst - Projekte
    (mein letzter Commit am SA hat wohl nicht geklappt?)

--- include/projekte.php
+++ include/projekte.php
@@ -10,7 +10,6 @@
     <div class="row">
         <div class="col-lg-12 text-center">
             <ul class="cbp-rfgrid">
-
                 <li>
                     <a data-toggle="modal" data-target="#modalbasic" data-projekt-id="mittelm" title="Projektdetails öffnen">
                     <img class="lazy" data-src="img/projekte/project_mittelm.jpg" title="SENSEVEN Projekt von Annika Mittelmeier" alt="SENSEVEN Projekt von Annika Mittelmeier" /><div><h3>Annika Mittelmeier</h3></div></a>
@@ -18,28 +17,25 @@
                 <li>
                     <a data-toggle="modal" data-target="#modalbasic" data-projekt-id="bardosi" title="Projektdetails öffnen">
                     <img class="lazy" data-src="img/projekte/project_bardosi.jpg" alt="SENSEVEN Projekt von Vivien Bardosi" title="SENSEVEN Projekt von Vivien Bardosi" /><div><h3>Vivien Bardosi</h3></div></a>
-                </li>
-                
+                </li> 
                 <li>
                 	<a data-toggle="modal" data-target="#modalbasic" data-projekt-id="biersack" title="Projektdetails öffnen">
                     <img class="lazy" data-src="img/projekte/project_biersack.jpg" alt="SENSEVEN Projekt von Teresa Biersack" title="SENSEVEN Projekt von Teresa Biersack"/><div><h3>Teresa Biersack</h3></div></a>
                 </li> 
-
                 <li>
                 	<a data-toggle="modal" data-target="#modalbasic" data-projekt-id="frohwein" title="Projektdetails öffnen">
                     <img class="lazy" data-src="img/projekte/project_frohwein.jpg" alt="SENSEVEN Projekt von Daniel Frohwein" title="SENSEVEN Projekt von Daniel Frohwein"/><div><h3>Daniel Frohwein</h3></div></a>
-                </li> 
-                    
+                </li>                    
                 <li>
                 	<a data-toggle="modal" data-target="#modalbasic" data-projekt-id="heinemann" title="Projektdetails öffnen">
                     <img class="lazy" data-src="img/projekte/project_heinemann.jpg" alt="SENSEVEN Projekt von Michèle Heinemann" title="SENSEVEN Projekt von Michèle Heinemann"/><div><h3>Michèle Heinemann</h3></div></a>
                 </li>
                 <li>
-                	<a class="empty hidden-xs hidden-sm hidden-md hidden-lg visible-lgplus"><img class="lazy" data-src="img/projekte/project_empty.png" /></a>
+                	<a class="empty hidden-xs hidden-sm hidden-md hidden-lg visible-lgplus"><img class="lazy" data-src="img/projekte/project_empty.png" alt="Projektvorschau" /></a>
                 </li>  
                 <li>
                 	<a data-toggle="modal" data-target="#modalbasic" data-projekt-id="dittrich" title="Projektdetails öffnen">
-                    <img class="lazy" data-src="img/projekte/project_dittrich.jpg" alt="SENSEVEN Projekt von Svenja Dittrich" title="SENSEVEN Projekt von Svenja Dittrich"/><div><h3>Svenja Dittrich</h3></div></a>
+                    <img class="lazy" data-src="img/projekte/project_dittrich.jpg" alt="SENSEVEN Projekt von Svenja Dittrich" title="SENSEVEN Projekt von Svenja Dittrich" /><div><h3>Svenja Dittrich</h3></div></a>
                 </li>  
                 <li>
                 	<a data-toggle="modal" data-target="#modalbasic" data-projekt-id="kurbanova" title="Projektdetails öffnen">
@@ -52,13 +48,11 @@
                 <li>
                 	<a data-toggle="modal" data-target="#modalbasic" data-projekt-id="redeker" title="Projektdetails öffnen">
                     <img class="lazy" data-src="img/projekte/project_redeker.jpg" alt="SENSEVEN Projekt von Laura Redeker" title="SENSEVEN Projekt von Laura Redeker"/><div><h3>Laura Redeker</h3></div></a>
-                </li>   
-                 
+                </li>                    
                 <li>
                 	<a data-toggle="modal" data-target="#modalbasic" data-projekt-id="wang" title="Projektdetails öffnen">
                     <img class="lazy" data-src="img/projekte/project_wang.jpg" alt="SENSEVEN Projekt von Robin Wang" title="SENSEVEN Projekt von Robin Wang" /><div><h3>Robin Wang</h3></div></a>
                 </li>  
-
                 <li>
                 	<a data-toggle="modal" data-target="#modalbasic" data-projekt-id="binder" title="Projektdetails öffnen">
                     <img class="lazy" data-src="img/projekte/project_binder.jpg" alt="SENSEVEN Projekt von Katharina Binder" title="SENSEVEN Projekt von Katharina Binder"/><div><h3>Katharina Binder</h3></div></a>
@@ -69,7 +63,7 @@
                 </li>
                 <li>
                 	<a data-toggle="modal" data-target="#modalbasic" data-projekt-id="korbanka" title="Projektdetails öffnen">
-                    <img class="lazy" data-src="img/projekte/project_korbanka.jpg" alt="SENSEVEN Projekt von Lasse Korbanka" title="SENSEVEN Projekt von Lasse Korbanka"/><div><h3>Lasse Korbanka</h3></div></a>
+                    <img class="lazy" data-src="img/projekte/project_korbanka.jpg" alt="SENSEVEN Projekt von Lasse Korbanka" title="SENSEVEN Projekt von Lasse Korbanka" /><div><h3>Lasse Korbanka</h3></div></a>
                 </li>              
                 <li>
                 	<a data-toggle="modal" data-target="#modalbasic" data-projekt-id="scavuzzo" title="Projektdetails öffnen">
@@ -77,14 +71,14 @@
                 </li> 
                 <li>
                 	<a data-toggle="modal" data-target="#modalbasic" data-projekt-id="rapp" title="Projektdetails öffnen">
-                    <img class="lazy" data-src="img/projekte/project_rapp.jpg" alt="SENSEVEN Projekt von Padraic Rapp" title="SENSEVEN Projekt von Padraic Rapp"/><div><h3>Padraic Rapp</h3></div></a>
+                    <img class="lazy" data-src="img/projekte/project_rapp.jpg" alt="SENSEVEN Projekt von Padraic Rapp" title="SENSEVEN Projekt von Padraic Rapp" /><div><h3>Padraic Rapp</h3></div></a>
                 </li>
                 <li>
-                	<a class="empty hidden-xs hidden-sm hidden-md hidden-lg visible-lgplus"><img class="lazy" data-src="img/projekte/project_empty.png" /></a>
+                	<a class="empty  hidden-xs hidden-sm hidden-md hidden-lg visible-lgplus"><img class="lazy" data-src="img/projekte/project_empty.png" alt="Projektvorschau"/></a>
                 </li>   
                 <li>
                 	<a data-toggle="modal" data-target="#modalbasic" data-projekt-id="woods" title="Projektdetails öffnen">
-                    <img class="lazy" data-src="img/projekte/project_woods.jpg" alt="SENSEVEN Projekt von Annika Mittelmeier" title="SENSEVEN Projekt von Annika Mittelmeier"/><div><h3>Jessica Woods</h3></div></a>
+                    <img class="lazy" data-src="img/projekte/project_woods.jpg" alt="SENSEVEN Projekt von Jessica Woods" title="SENSEVEN Projekt von Jessica Woods" /><div><h3>Jessica Woods</h3></div></a>
                 </li>          
                 <li>
                 	<a data-toggle="modal" data-target="#modalbasic" data-projekt-id="schmidt" title="Projektdetails öffnen">
